f16a147add2bd627a4bc36dc0a74d851684f6068
Nathan Miranda <nate845@gmail.com>
2019-03-26 00:21:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function onKeyDown(event) {
     // currentPosition -> 180 ('180px' -> 180 from parseInt)
     dodger.style.left = currentPosition - 5 + 'px' // (175px)
   } else if (event.keyCode === 39) {
-    
+    const currentPosition = parseInt(dodger.style.left)
+    // currentPosition -> 180 ('180px' -> 180 from parseInt)
+    dodger.style.left = currentPosition + 5 + 'px' // (175px)
   }
 }
 
