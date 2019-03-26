e97af7832cad2a2af34d3f8f84f782dc7080961b
Nathan Miranda <nate845@gmail.com>
2019-03-26 00:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dodger = document.getElementById('dodger')
 function onKeyDown(event) {
   if (event.keyCode === 37) {
     // how do we actually move this left  
+    const currentPosition = parseInt()
     dodger.style.left = currentPosition - 5 // (180px)
     
   }
